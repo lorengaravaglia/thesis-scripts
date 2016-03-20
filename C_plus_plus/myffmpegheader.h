56bac58a160db634df6e97a9e1e0053155f0d571
lorengaravaglia <loren.garavaglia@gmail.com>
2016-03-19 20:50:21 -0400

    Added files via upload

---
+++ C_plus_plus/myffmpegheader.h
@@ -0,0 +1,17 @@
+#define __STDC_CONSTANT_MACROS
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavutil/pixfmt.h"
+}
+
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "avutil.lib")
+
+extern AVCodecContext		 *pCodecCtx;
+extern char 				 filepath[];
+extern int				     videoindex;
