7e34c57cedbb7eb952ccbe4aae1dc388393678de
lorengaravaglia <loren.garavaglia@gmail.com>
2016-04-02 23:05:08 -0400

    Added files via upload

--- C_plus_plus/FFMPEG Control.cpp
+++ C_plus_plus/FFMPEG Control.cpp
@@ -24,11 +24,22 @@
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
-//#include <time.h>
 
 using namespace std;
 
+int sleepTime = 30000;
+
 int main(int argc, const char *argv[]) {
+
+	// This structure will be used to create the keyboard
+    // input event.
+    INPUT ip;
+ 
+    // Set up a generic keyboard event.
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
 
 	STARTUPINFO si, si1;
     PROCESS_INFORMATION pi, pi1;
@@ -39,9 +50,10 @@ int main(int argc, const char *argv[]) {
 	si1.cb = sizeof(si1);
     ZeroMemory( &pi, sizeof(pi) );
 	ZeroMemory( &pi1, sizeof(pi1) );
-	const TCHAR input[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 5 -vf format=gray -an -f rtp rtp://127.0.0.1:1234");
+	const TCHAR input[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 10 -vf format=gray -b:v 500k -bufsize -1000k -an -f rtp rtp://127.0.0.1:1234");
 	//const TCHAR input[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 30 -an -f rtp rtp://127.0.0.1:1234");
-	const TCHAR input1[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 5 -vf format=gray -crf 40 -an -f rtp rtp://127.0.0.1:1234");
+	
+	const TCHAR input1[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 10 -vf format=gray -crf 45 -an -f rtp rtp://127.0.0.1:1234");   //-b:v 32k -bufsize -64k
 	//G:\\Master's Thesis Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.avi
     // Start the child process. 
 
@@ -63,84 +75,43 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
-	cout<<"first process created, sleep for 20 seconds"<<endl;
-	Sleep(40000);
+	cout<<"first process created, sleep."<<endl;
+	Sleep(sleepTime);
 	cout<<"done sleeping"<<endl;
-	//int j = 1;
+	
+	for(;;)
+	{
 
-    // Get a handle to the Video device:
-    
-	/*
-	cap.open("C:\\Users\\Loren\\Documents\\Visual Studio 2012\\Projects\\OpencvSetup\\OpencvSetup\\test.sdp");
-	cout<<"stream opened"<<endl;
-    // Check if we can use this device at all:
-    if(!cap.isOpened()) {
-        cerr << "Capture Device ID cannot be opened." << endl;
-        return -1;
-    }
-	*/
+		TerminateProcess(pi.hProcess, NULL);
 
-    // Holds the current frame from the Video device:
-    //Mat frame;
-    
-	for(;;) {
-		//cap >> frame;
-
-		//if(j%6600 == 0)
-		//{
-
-			TerminateProcess(pi.hProcess, NULL);
-
-			CreateProcess( NULL,   // No module name (use command line)
-						  (LPWSTR)input1,        // Command line
-						  NULL,           // Process handle not inheritable
-						  NULL,           // Thread handle not inheritable
-						  FALSE,          // Set handle inheritance to FALSE
-						  0,              // No creation flags
-						  NULL,           // Use parent's environment block
-						  NULL,           // Use parent's starting directory 
-						  &si1,            // Pointer to STARTUPINFO structure
-						  &pi1);
-			Sleep(60000);
+		CreateProcess( NULL,   // No module name (use command line)
+					  (LPWSTR)input1,        // Command line
+					  NULL,           // Process handle not inheritable
+					  NULL,           // Thread handle not inheritable
+					  FALSE,          // Set handle inheritance to FALSE
+					  0,              // No creation flags
+					  NULL,           // Use parent's environment block
+					  NULL,           // Use parent's starting directory 
+					  &si1,            // Pointer to STARTUPINFO structure
+					  &pi1);
+		
+		Sleep(sleepTime);
 
 		TerminateProcess(pi1.hProcess, NULL);
 
-			CreateProcess( NULL,   // No module name (use command line)
-						  (LPWSTR)input,        // Command line
-						  NULL,           // Process handle not inheritable
-						  NULL,           // Thread handle not inheritable
-						  FALSE,          // Set handle inheritance to FALSE
-						  0,              // No creation flags
-						  NULL,           // Use parent's environment block
-						  NULL,           // Use parent's starting directory 
-						  &si,            // Pointer to STARTUPINFO structure
-						  &pi);
-			Sleep(60000);
+		CreateProcess( NULL,   // No module name (use command line)
+					  (LPWSTR)input,        // Command line
+					  NULL,           // Process handle not inheritable
+					  NULL,           // Thread handle not inheritable
+					  FALSE,          // Set handle inheritance to FALSE
+					  0,              // No creation flags
+					  NULL,           // Use parent's environment block
+					  NULL,           // Use parent's starting directory 
+					  &si,            // Pointer to STARTUPINFO structure
+					  &pi);
 
+		Sleep(sleepTime);
 
-			//cap.open("C:\\Users\\Loren\\Documents\\Visual Studio 2012\\Projects\\OpencvSetup\\OpencvSetup\\test.sdp");
-			//cout<<"stream opened"<<endl;
-		//}
-		//else if(j %3300 == 0)
-		//{
-
-			//cap.open("C:\\Users\\Loren\\Documents\\Visual Studio 2012\\Projects\\OpencvSetup\\OpencvSetup\\test.sdp");
-			//cout<<"stream opened"<<endl;
-			//j = 0;
-		//}
-		/*
-		//int bytes = frame.total()*frame.elemSize();
-		//cout<<"size = "<<bytes<<endl;
-		// Show the result:
-	    cv::imshow("face_recognizer", frame);
-		// And display it:
-	    char key = (char) waitKey(20);
-		// Exit this loop on escape:
-		if(key == 27)
-			break;
-		*/
-		//j++;
-		//cout<<j<<endl;
-		}
+	}
     return 0;
 }
