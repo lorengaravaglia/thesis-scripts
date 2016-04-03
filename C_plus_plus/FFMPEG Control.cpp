061e648760f3b09731640a5999a6d26cffc0fd50
lorengaravaglia <loren.garavaglia@gmail.com>
2016-04-03 13:05:09 -0400

    Added files via upload

--- C_plus_plus/FFMPEG Control.cpp
+++ C_plus_plus/FFMPEG Control.cpp
@@ -31,15 +31,9 @@ int sleepTime = 30000;
 
 int main(int argc, const char *argv[]) {
 
-	// This structure will be used to create the keyboard
-    // input event.
-    INPUT ip;
- 
-    // Set up a generic keyboard event.
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0; // hardware scan code for key
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
+	HANDLE hPipe;
+	char buffer[1024];
+	DWORD dwRead;
 
 	STARTUPINFO si, si1;
     PROCESS_INFORMATION pi, pi1;
@@ -56,62 +50,74 @@ int main(int argc, const char *argv[]) {
 	const TCHAR input1[] = TEXT("ffmpeg -f concat -re -i G:\\Masters_Thesis_Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.txt -vcodec libx264 -g 10 -vf format=gray -crf 45 -an -f rtp rtp://127.0.0.1:1234");   //-b:v 32k -bufsize -64k
 	//G:\\Master's Thesis Files\\Honda_Database\\Database1\\Training\\videos\\behzad\\behzad.avi
     // Start the child process. 
-
-
-
-	if( !CreateProcess( NULL,   // No module name (use command line)
-        (LPWSTR)input,        // Command line  (LPWSTR)input
-        NULL,           // Process handle not inheritable
-        NULL,           // Thread handle not inheritable
-        FALSE,          // Set handle inheritance to FALSE
-        0,              // No creation flags
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi )           // Pointer to PROCESS_INFORMATION structure
-    ) 
-    {
-        printf( "CreateProcess failed (%d).\n", GetLastError() );
-        return 0;
-    }
-
-	cout<<"first process created, sleep."<<endl;
-	Sleep(sleepTime);
-	cout<<"done sleeping"<<endl;
 	
-	for(;;)
-	{
-
-		TerminateProcess(pi.hProcess, NULL);
-
-		CreateProcess( NULL,   // No module name (use command line)
-					  (LPWSTR)input1,        // Command line
-					  NULL,           // Process handle not inheritable
-					  NULL,           // Thread handle not inheritable
-					  FALSE,          // Set handle inheritance to FALSE
-					  0,              // No creation flags
-					  NULL,           // Use parent's environment block
-					  NULL,           // Use parent's starting directory 
-					  &si1,            // Pointer to STARTUPINFO structure
-					  &pi1);
+	hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\Pipe"),
+		PIPE_ACCESS_DUPLEX | PIPE_TYPE_BYTE | PIPE_READMODE_BYTE,   // FILE_FLAG_FIRST_PIPE_INSTANCE is not needed but forces CreateNamedPipe(..) to fail if the pipe already exists...
+		PIPE_WAIT,
+		1,
+		1024 * 16,
+		1024 * 16,
+		NMPWAIT_USE_DEFAULT_WAIT,
+		NULL);
+	
+	//while (TRUE)
+	//{
+	if (!CreateProcess(NULL,   // No module name (use command line)
+			(LPWSTR)input,        // Command line  (LPWSTR)input
+			NULL,           // Process handle not inheritable
+			NULL,           // Thread handle not inheritable
+			FALSE,          // Set handle inheritance to FALSE
+			0,              // No creation flags
+			NULL,           // Use parent's environment block
+			NULL,           // Use parent's starting directory 
+			&si,            // Pointer to STARTUPINFO structure
+			&pi)           // Pointer to PROCESS_INFORMATION structure
+			)
+		{
+			printf("CreateProcess failed (%d).\n", GetLastError());
+			return 0;
+		}
 		
-		Sleep(sleepTime);
+		while (hPipe != INVALID_HANDLE_VALUE)
+		{
+			printf("waiting for named pipe\n");
+			if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
+			{
+				printf("connect pipe != to false\n");
+				//while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
+				while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
+				{
+					// add terminating zero
+					buffer[dwRead] = '\0';
+					printf("got data from named pipe, checking the value.\n");
+					if (strcmp(buffer, "1") == 0)
+					{
+						printf("data from named pipe did match string comparison.\n");
+						TerminateProcess(pi.hProcess, NULL);
 
-		TerminateProcess(pi1.hProcess, NULL);
+						if (!CreateProcess(NULL,   // No module name (use command line)
+							(LPWSTR)input,        // Command line  (LPWSTR)input
+							NULL,           // Process handle not inheritable
+							NULL,           // Thread handle not inheritable
+							FALSE,          // Set handle inheritance to FALSE
+							0,              // No creation flags
+							NULL,           // Use parent's environment block
+							NULL,           // Use parent's starting directory 
+							&si,            // Pointer to STARTUPINFO structure
+							&pi)           // Pointer to PROCESS_INFORMATION structure
+							)
+						{
+							printf("CreateProcess failed (%d).\n", GetLastError());
+							return 0;
+						}
+					}
+					// do something with data in buffer 
+					printf("%s\n", buffer);
+				}
+			}	
+		}
+	//}
 
-		CreateProcess( NULL,   // No module name (use command line)
-					  (LPWSTR)input,        // Command line
-					  NULL,           // Process handle not inheritable
-					  NULL,           // Thread handle not inheritable
-					  FALSE,          // Set handle inheritance to FALSE
-					  0,              // No creation flags
-					  NULL,           // Use parent's environment block
-					  NULL,           // Use parent's starting directory 
-					  &si,            // Pointer to STARTUPINFO structure
-					  &pi);
-
-		Sleep(sleepTime);
-
-	}
+	DisconnectNamedPipe(hPipe);
     return 0;
 }
