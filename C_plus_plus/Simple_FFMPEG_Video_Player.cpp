3f7bff229ee993e025a636e81426bd4474484146
lorengaravaglia <loren.garavaglia@gmail.com>
2016-04-05 21:35:13 -0400

    Added files via upload

--- C_plus_plus/Simple_FFMPEG_Video_Player.cpp
+++ C_plus_plus/Simple_FFMPEG_Video_Player.cpp
@@ -23,35 +23,105 @@ extern "C"
 #pragma comment(lib, "SDL2.lib")
 #pragma comment(lib, "SDL2main.lib") 
 
+struct FFMPEGData {
 	AVFormatContext	*pFormatCtx;
-	int				 videoindex;
-	unsigned int     i;
 	AVCodecContext	*pCodecCtx;
 	AVCodec			*pCodec;
-	AVFrame			*pFrame;
-	uint8_t			*out_buffer;
+	AVFrame			*pFrame, dst;
 	AVPacket		*packet;
-	AVFrame			dst;
-	int				ret, got_picture;
-	cv::Mat			m;
 
 	struct SwsContext *convert_ctx;
-	
+
 	enum PixelFormat dst_pixfmt = PIX_FMT_GRAY8; //full color image.
-	char filepath[]= "test.sdp"; 
-	const char hello[] = "1";
 
-	HANDLE hPipe;
-	DWORD dwWritten = 0;
-	clock_t begin, end;
+	uint8_t			*out_buffer;
+	int				got_picture, videoindex;
+};
 
-	int randBase = 30;
+cv::Mat m;
 
-	int randValue = 0;
+char filepath[] = "test.sdp";
+const char hello[] = "1";
+
+HANDLE hPipe;
+DWORD dwWritten = 0;
+clock_t begin, end;
+
+int ret;
+int randBase = 30;
+int randValue = 0;
+
+
+int startFFMPEG(FFMPEGData &data)
+{
+	data.pFormatCtx = avformat_alloc_context();
+
+	if (avformat_open_input(&data.pFormatCtx, filepath, NULL, NULL) < 0)
+	{
+		printf("Couldn't open input stream.\n");
+		return -1;
+	}
+
+	if (avformat_find_stream_info(data.pFormatCtx, NULL) < 0)
+	{
+		printf("Couldn't find stream information.\n");
+		return -1;
+	}
+
+	data.videoindex = -1;
+	for (unsigned int i = 0; i < data.pFormatCtx->nb_streams; i++)
+	{
+		if (data.pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			data.videoindex = i;
+			break;
+		}
+	}
+
+	if (data.videoindex == -1)
+	{
+		printf("Didn't find a video stream.\n");
+		return -1;
+	}
+
+	data.pCodecCtx = data.pFormatCtx->streams[data.videoindex]->codec;
+	data.pCodec = avcodec_find_decoder(data.pCodecCtx->codec_id);
+
+	if (data.pCodec == NULL)
+	{
+		printf("Codec not found.\n");
+		return -1;
+	}
+
+	if (avcodec_open2(data.pCodecCtx, data.pCodec, NULL) < 0)
+	{
+		printf("Could not open codec.\n");
+		return -1;
+	}
+
+	data.pFrame = av_frame_alloc();
+
+	data.out_buffer = (uint8_t *)av_malloc(avpicture_get_size(data.dst_pixfmt, data.pCodecCtx->width, data.pCodecCtx->height));
+
+	avpicture_fill((AVPicture *)&data.dst, data.out_buffer, data.dst_pixfmt, data.pCodecCtx->width, data.pCodecCtx->height);
+
+	data.packet = (AVPacket *)av_malloc(sizeof(AVPacket));
+
+	//Output Info-----------------------------
+	printf("--------------- File Information ----------------\n");
+	av_dump_format(data.pFormatCtx, 0, filepath, 0);
+	printf("-------------------------------------------------\n");
+
+	data.convert_ctx = sws_getContext(data.pCodecCtx->width, data.pCodecCtx->height, data.pCodecCtx->pix_fmt,
+		data.pCodecCtx->width, data.pCodecCtx->height, data.dst_pixfmt, SWS_FAST_BILINEAR, NULL, NULL, NULL);
+
+	return 0;
+}
+
 
 int main(int argc, char **argv)
 {
-
+	FFMPEGData vidData;
 
 	av_register_all();
 	avformat_network_init();
@@ -68,96 +138,42 @@ int main(int argc, char **argv)
 	{
 		randValue = rand() % 100 + randBase;
 
+		if (startFFMPEG(vidData) < 0)
+		{
+			printf("should not get here.\n");
+			return 0;
+		}
+
 		printf("time for this loop = %d", randValue);
-		
-		pFormatCtx = avformat_alloc_context();
-
-		if (avformat_open_input(&pFormatCtx, filepath, NULL, NULL) < 0)
-		{
-			printf("Couldn't open input stream.\n");
-			return -1;
-		}
-
-		if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
-		{
-			printf("Couldn't find stream information.\n");
-			return -1;
-		}
-
-		videoindex = -1;
-		for (i = 0; i < pFormatCtx->nb_streams; i++)
-		{
-			if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
-			{
-				videoindex = i;
-				break;
-			}
-		}
-
-		if (videoindex == -1)
-		{
-			printf("Didn't find a video stream.\n");
-			return -1;
-		}
-
-		pCodecCtx = pFormatCtx->streams[videoindex]->codec;
-		pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
-
-		if (pCodec == NULL)
-		{
-			printf("Codec not found.\n");
-			return -1;
-		}
-
-		if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
-		{
-			printf("Could not open codec.\n");
-			return -1;
-		}
-
-		pFrame = av_frame_alloc();
-
-		out_buffer = (uint8_t *)av_malloc(avpicture_get_size(dst_pixfmt, pCodecCtx->width, pCodecCtx->height));
-
-		avpicture_fill((AVPicture *)&dst, out_buffer, dst_pixfmt, pCodecCtx->width, pCodecCtx->height);
-
-		packet = (AVPacket *)av_malloc(sizeof(AVPacket));
-
-		//Output Info-----------------------------
-		printf("--------------- File Information ----------------\n");
-		av_dump_format(pFormatCtx, 0, filepath, 0);
-		printf("-------------------------------------------------\n");
-
-		convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
-			pCodecCtx->width, pCodecCtx->height, dst_pixfmt, SWS_FAST_BILINEAR, NULL, NULL, NULL);
 
 		begin = clock();
 
 		while (true)
 		{
-			if (av_read_frame(pFormatCtx, packet) != AVERROR(EAGAIN))
+			if (av_read_frame(vidData.pFormatCtx, vidData.packet) != AVERROR(EAGAIN))
 			{
-				if (packet->stream_index == videoindex)
+				if (vidData.packet->stream_index == vidData.videoindex)
 				{
-					ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
+					ret = avcodec_decode_video2(vidData.pCodecCtx, vidData.pFrame, &vidData.got_picture, vidData.packet);
 					if (ret < 0)
 					{
 						printf("Decode Error.\n");
 						return -1;
 					}
 
-					if (got_picture)
+					if (vidData.got_picture)
 					{
-						if (convert_ctx == NULL)
+						if (vidData.convert_ctx == NULL)
 						{
 							fprintf(stderr, "Cannot initialize the conversion context!\n");
 							exit(1);
 						}
 
-						sws_scale(convert_ctx, pFrame->data, pFrame->linesize, 0, pFrame->height,
-							dst.data, dst.linesize);
+						sws_scale(vidData.convert_ctx, vidData.pFrame->data, vidData.pFrame->linesize, 0,
+							vidData.pFrame->height, vidData.dst.data, vidData.dst.linesize);
 
-						m = cv::Mat(pFrame->height, pFrame->width, CV_8UC1, dst.data[0], dst.linesize[0]);
+						m = cv::Mat(vidData.pFrame->height, vidData.pFrame->width, CV_8UC1,
+							vidData.dst.data[0], vidData.dst.linesize[0]);
 
 						cv::imshow("MyVid", m);
 						cv::waitKey(20);
@@ -171,25 +187,24 @@ int main(int argc, char **argv)
 							{
 								WriteFile(hPipe,
 									hello,				//"Hello Pipe\n",
-									sizeof(hello),								//12,   // = length of string + terminating '\0' !!!
+									sizeof(hello),		// = length of string + terminating '\0' !!!
 									&dwWritten,
 									NULL);
 								printf("wrote to pipe\n");
 								//CloseHandle(hPipe);
 							}
-							//Sleep(1000);
 							break;
 						}
 					}
 				}
-				av_free_packet(packet);
+				av_free_packet(vidData.packet);
 			}
 		}
-		av_free_packet(packet);
-		sws_freeContext(convert_ctx);
-		av_frame_free(&pFrame);
-		avcodec_close(pCodecCtx);
-		avformat_close_input(&pFormatCtx);
+		av_free_packet(vidData.packet);
+		sws_freeContext(vidData.convert_ctx);
+		av_frame_free(&vidData.pFrame);
+		avcodec_close(vidData.pCodecCtx);
+		avformat_close_input(&vidData.pFormatCtx);
 	}
 	return 0;
 }
